e623d715941ce467235d4f1936c09ce6a16b0834
Hajiyevrehman <haciyevrehman09@gmail.com>
2025-03-11 17:57:37 -0700

    cleaning tests

--- tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-# Add the project root directory to Python's module search path
-# Going up one level from tests directory to get to project root
-sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
